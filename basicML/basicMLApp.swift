570a9c4b2b3bf8345053174cf2d34faca016bd16
jlindell <jakob.lindell@gmail.com>
2023-10-21 10:50:21 +0200

    Initial Commit

---
+++ basicML/basicMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicMLApp.swift
+//  basicML
+//
+//  Created by Jakob Lindell on 2023-10-21.
+//
+
+import SwiftUI
+
+@main
+struct basicMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
